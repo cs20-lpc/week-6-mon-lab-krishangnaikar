0851def80d3b15ff2db8e0ecdf865e04dd1b0e7b
Krishang Naikar <krishangnaikar7@gmail.com>
2026-02-25 15:41:35 -0800

    sub 2

---
+++ part-a.cpp
@@ -0,0 +1,86 @@
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+/*******************************************************************************
+ * Function prototype
+*******************************************************************************/
+
+template <typename T>
+// Complexity: O(n) time, O(n) space
+T findMaxRecTail(const T* arr, const int size, int index = 0)
+{
+    if (index == size - 1) {
+        return arr[index];
+    }
+    T max = findMaxRecTail(arr, size, index + 1);
+    return (arr[index] > max) ? arr[index] : max;
+}
+
+template <typename T>
+T findMaxRecBinarySplit(const T* arr, const int left, const int right)
+{
+    if (left == right) {
+        return arr[left];
+    }
+    int mid = left + (right - left) / 2;
+    T maxLeft = findMaxRecBinarySplit(arr, left, mid);
+    T maxRight = findMaxRecBinarySplit(arr, mid + 1, right);
+    return (maxLeft > maxRight) ? maxLeft : maxRight;
+}
+/*******************************************************************************
+ * Description:
+ * Starting point of the program. Creates two arrays, one fixed and the other
+ * random. Determines the maximum value by calling the local function and the
+ * standard function.
+ * 
+ * Input:
+ * N/A
+ *
+ * Output:
+ * An integer to signal to the OS the exit code.
+*******************************************************************************/
+
+int main() {
+    // create the array
+    const int SIZE = 10;
+    int myArray[SIZE] = {
+        5, 23, 0, -2, 4,
+        9, 11, 21, 130, 6
+    };
+
+    // display the maximum
+    cout << setfill('-') << setw(40) << "" << endl;
+    cout << "Maximum using Recursion: "
+         << findMaxRecTail(myArray, SIZE) << endl 
+         << "From Binary split:" 
+        << findMaxRecBinarySplit(myArray, 0, SIZE-1)
+         << "Should be 130 for the fixed array\n";
+    cout << setfill('-') << setw(40) << "" << endl << endl;
+
+    // create a random array
+    const int SIZE_2 = 1000;
+    const int MAX_VAL = 10000;
+    int randArray[SIZE_2];
+    srand(time(0));
+    for (int& elem : randArray) {
+        elem = rand() % MAX_VAL;
+    }
+
+    // find the maximum using C++ standard's algorithm
+    cout << setfill('-') << setw(40) << "" << endl;
+    cout << "Maximum using Standard Algorithm: "
+         << *max_element(randArray, randArray + SIZE_2) << endl;
+
+    // find the maximum using the recursive implementation
+    cout << "Maximum using Recursion: "
+         << findMaxRecTail(randArray, SIZE_2) << endl;
+    cout << setfill('-') << setw(40) << "" << endl;
+
+    // terminate
+    return 0;
+}
+
---
+++ part-b.cpp
@@ -0,0 +1,48 @@
+#include <iomanip>
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*******************************************************************************
+ * Function prototype
+*******************************************************************************/
+
+void towerHanoi(int, string, string, string, int&);
+
+/*******************************************************************************
+ * Description:
+ * Starting point of the program. Calls a function to recursively determine
+ * solutions for the first 5 instances of the Tower of Hanoi problem.
+ * 
+ * Input:
+ * N/A
+ *
+ * Output:
+ * An integer to signal to the OS the exit code.
+*******************************************************************************/
+
+void towerHanoi(int n, string fromRod, string toRod, string auxRod, int& numMoves)
+{
+    if (n == 1) {
+        cout << "Moving disk 1 from " << fromRod << " to " << toRod << endl;
+        numMoves++;
+        return;
+    }
+    towerHanoi(n - 1, fromRod, auxRod, toRod, numMoves);
+    cout << "Move disk " << n << " from " << fromRod << " to " << toRod << endl;
+    numMoves++;
+    towerHanoi(n - 1, auxRod, toRod, fromRod, numMoves);
+}
+
+int main() {
+    // solve the first 5 cases of the Tower of Hanoi problem
+    for (int i = 1, numMoves = 0; i <= 5; i++, numMoves = 0) {
+        cout << setfill('-') << setw(40) << "" << endl;
+        cout << "Trying n = " << i << endl << endl;
+        towerHanoi(i, "Rod #1", "Rod #2", "Rod #3", numMoves);
+        cout << endl << "Number of Moves = " << numMoves << endl;
+    }
+
+    // terminate
+    return 0;
+}
